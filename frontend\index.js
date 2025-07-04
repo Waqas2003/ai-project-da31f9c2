6a97d7e28dfb51fc97cd177f3d360209e03fa30a
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 22:17:50 +0500

    Add frontend\index.js

---
+++ frontend\index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProductList from './pages/ProductList';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ProductList />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
